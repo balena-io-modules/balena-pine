3f68368659c8424995daa2d2223152a8779ee0c3
Thodoris Greasidis <thgreasi@gmail.com>
2020-06-05 16:44:35 +0300

    Convert karma.conf to js
    Change-type: patch
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- karma.conf.coffee
+++
@@ -1,8 +0,0 @@
-getKarmaConfig = require('balena-config-karma')
-packageJSON = require('./package.json')
-
-getKarmaConfig.DEFAULT_WEBPACK_CONFIG.externals = fs: true
-
-module.exports = (config) ->
-	karmaConfig = getKarmaConfig(packageJSON)
-	config.set(karmaConfig)
---
+++ karma.conf.js
@@ -0,0 +1,13 @@
+var getKarmaConfig = require('balena-config-karma');
+
+var packageJSON = require('./package.json');
+
+getKarmaConfig.DEFAULT_WEBPACK_CONFIG.externals = {
+  fs: true
+};
+
+module.exports = function(config) {
+  var karmaConfig;
+  karmaConfig = getKarmaConfig(packageJSON);
+  return config.set(karmaConfig);
+};
